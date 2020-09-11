cb3689588d8e2919520ea5ff2c5cbf6940566d38
scottpeters <peters.scottp@gmail.com>
2020-09-11 12:30:47 -0500

    Transfered Files from Old Project

---
+++ McDonalds/DriveThru.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace McDonalds
+{
+    class DriveThru
+    {
+        private float Number1 = 5f;
+        private float Number2 = 3f;
+        private float HotNSpicy = 1.09f;
+        public float Tenpiece = 7.50f;
+
+        /*  Order
+
+            1. First, check what the menu number was input by the customer
+            2. Second, return the price of the menu number that the customer chose
+            3. Third, add the price menu number to my total cost
+            4. Last, give the customer the  total price for everything he ordered
+
+        */
+
+        
+        public float Order(char menuNumber)
+
+        {
+            if (menuNumber == '1')
+            {
+                return Number1;
+            }
+            if (menuNumber == '2')
+            {
+                return Number2;
+            }
+            if (menuNumber == '3')
+            {
+                return HotNSpicy;
+            }
+            if (menuNumber == '4')
+            {
+                return Tenpiece;
+            }
+            
+
+         
+                
+
+            
+            return 0; //change this later
+        }
+
+        
+
+            
+
+
+
+        
+
+        public bool IceCreamMachineBroke()
+        {
+            return true;
+
+
+        }
+         private string ChickenWings()
+        {
+            string FlavorHotBBQ = "Hot BBQ";
+            return FlavorHotBBQ;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+    }
+}
---
+++ McDonalds/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace McDonalds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveThru McDonaldsDriveThru = new DriveThru();
+            float TotalCost = 0;
+            Console.WriteLine("Welcome to McDonald's what will you be having today?");
+            Console.WriteLine("Number 1: $5");
+            Console.WriteLine("Number 2: $3");
+            Console.WriteLine("Number 3: $1.09");
+            Console.WriteLine("Number 4: $7.50");
+            char input;
+            input = Console.ReadKey().KeyChar;
+            Console.WriteLine(System.Environment.NewLine);
+            //This is so i get a space after input
+
+            TotalCost = TotalCost + McDonaldsDriveThru.Order(input);
+            Console.WriteLine("Would this complete your order?");
+            Console.WriteLine("Press 1 for yes");
+            Console.WriteLine("Press 2 for no");
+            input = Console.ReadKey().KeyChar;
+
+            bool keepGoing = true;
+
+            while (keepGoing)
+            {
+
+            }
+            Console.WriteLine(TotalCost);
+            
+
+
+        }
+    }
+}
